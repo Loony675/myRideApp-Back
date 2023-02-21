c900b41717e768f6621474aae9998ca46ef4b67d
Loussot <theo.loussot@gmail.com>
2023-02-21 20:03:47 +0100

    add signIn

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/users");
-const uid2 = require('uid2')
-const bcrypt = require('bcrypt')
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 router.post("/signUp", function (req, res) {
-  User.findOne({ email: req.body.email}).then((data) => {
+  User.findOne({ email: req.body.email }).then((data) => {
     if (data) {
       res.json({ result: false, error: "User already exists" });
     } else {
@@ -13,12 +13,26 @@ router.post("/signUp", function (req, res) {
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10),
         username: req.body.username,
-        token: uid2(32)
+        token: uid2(32),
       });
       newUser.save().then((newUser) => {
-          res.json({ result: true, token: newUser.token, username: newUser.username });
+        res.json({
+          result: true,
+          token: newUser.token,
+          username: newUser.username,
+        });
       });
-    };
+    }
+  });
+});
+
+router.post("/signIn", (req, res) => {
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data && bcrypt.compareSync(res.body.password, data.password)) {
+      res.json({ result: true, username: data.username, token: data.token });
+    } else {
+      res.json({result:false, error:'User not found or wrong password'})
+    }
   });
 });
 
