dd0141a9b457c76d245d70fd81f6aceeab5e6bdf
Loussot <theo.loussot@gmail.com>
2023-02-25 18:26:50 +0100

    add schema moto user

--- models/users.js
+++ models/users.js
@@ -4,7 +4,16 @@ const usersSchema = mongoose.Schema({
     email: String,
     password: String,
     username:String,
-    token:String
+    token:String,
+    moto:[motoSchema]
+})
+
+const motoSchema = mongoose.Schema({
+    marque: String,
+    millesime: Number,
+    cylindree: Number,
+    modele: String,
+
 })
 
 const User = mongoose.model('users', usersSchema);
